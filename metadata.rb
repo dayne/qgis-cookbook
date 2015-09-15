c3c741f0e24fd20cf8578c30abe5cf539205970e
Dayne Broderson <dayne@alaska.edu>
2015-09-14 18:12:20 -0800

    rev version

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'broderson@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures qgis'
 long_description 'Installs/Configures qgis'
-version '0.1.1'
+version '0.1.2'
 source_url "https://github.com/dayne/qgis-cookbook"
 
 supports 'ubuntu', '>= 14.04'
 
-depends 'apt', '~> 2.7.0'
-depends 'chef-sugar', '~> 3.1.0'
+depends 'apt'
+depends 'chef-sugar'
 
