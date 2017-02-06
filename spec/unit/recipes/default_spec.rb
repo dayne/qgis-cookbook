fdb0dfa998754381e075b641444e02c0edb4e2a8
Dayne Broderson <broderson@gmail.com>
2017-02-05 17:19:50 -0900

    fixes tests

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,28 +6,18 @@
 
 require 'spec_helper'
 
-describe 'qgis::default' do
-  context 'When all attributes are default, on an ubuntu platform' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new.call(platform: 'ubuntu')
-      runner.converge(described_recipe)
-    end
+%w(14.04 16.04 16.10).each do |ver|
+  describe 'qgis::default' do
+    context "When all attributes are default, on an ubuntu #{ver} platform" do
+      let(:chef_run) do
+        runner = ChefSpec::ServerRunner.new(platform: 'ubuntu',
+                                            version: ver)
+        runner.converge(described_recipe)
+      end
 
-    it 'converges successfully' do
-      chef_run # This should not raise an error
-    end
-  end
-end
-
-describe 'qgis::centos' do
-  context 'When all attributes are default, on an ubuntu platform' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new.call(platform: 'centos')
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      chef_run # This should not raise an error
+      it 'converges successfully' do
+        chef_run # This should not raise an error
+      end
     end
   end
 end
